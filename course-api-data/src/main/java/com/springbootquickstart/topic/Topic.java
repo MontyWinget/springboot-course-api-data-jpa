a2aa728178075d8c3acef0b2039b5ec449f9ab4c
Monty Winget <monty.winget@gmail.com>
2021-05-25 13:33:49 +0530

    Entities and primary key annotations

--- course-api-data/src/main/java/com/springbootquickstart/topic/Topic.java
+++ course-api-data/src/main/java/com/springbootquickstart/topic/Topic.java
@@ -1,7 +1,12 @@
 package com.springbootquickstart.topic;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
 public class Topic {
 	
+	@Id
 	private String id;
 	private String name;
 	private String description;
@@ -41,7 +46,4 @@ public class Topic {
 	public void setDescription(String description) {
 		this.description = description;
 	}
-	
-	
-
 }
